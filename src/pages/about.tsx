713b4f91d5ef516be5e973b848ae8c7a6227c2d6
shuqi7 <shuqi7sq@gmail.com>
2019-03-09 23:07:16 -0800

    Fiex home page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
 export default () => (
-  <div>
-    <p>React Static is a progressive static site generator for React.</p>
+  <div className="about">
+    <p> Hi, this is Qi Shu. I'm currently a third-year student at the University of Waterloo, pursuing a Computer
+      Science and Business Administration Double Degree.
+      I love programming to solve problems, especially tackling issues which I might encouter in our daily lives. I'm
+      also interested in finance-related fields; I have always been
+      passionate about stock markets, and I hope I could have an opportunity to get in touch with it in the future.
+      :)
+    </p>
+    <br />
+    <p>
+      I worked as a web developer this winter at Ontario Investment Office, and was a research assistant in school
+      this summer in charge of developing a web application to compare people's
+      working efficiency, and analyzing data with NumPy. I will be starting another research
+      assistantship this fall, compiling DBMS that has a simple extensible runtime and operates
+      by translating SQL-like datamanipulation requests directly to C code that interfaces with
+      this runtime
+    </p>
+    <br />
+    <p>
+      I hope that I would have opportunities to work in different fields, be it in tech or finance, and explore
+      different companies' culture, and find where my true passion lies eventually.
+    </p>
+    <br />
+    <p>
+      If you should have any position which you find fit to me, please reach out to me through Email or LinkedIn.
+        Thanks!
+    </p>
   </div>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { withSiteData } from 'react-static'
 import "../styles/index.css"
 
-export default withSiteData(() => (
+export default () => (
   <div className={"home"}>
+    <h4>
+      Hey, I'm
+    </h4>
     <h1>
-      Welcome! This is Qi Shu
+      &mdash; Qi Shu &mdash;
     </h1>
-    <p>
-      A students who loves thinking, analyzing and designing at the University of Waterloo
-    </p>
   </div>
-))
+)
